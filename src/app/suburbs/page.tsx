26629268d388d7203dfb2aedc15915a159680f18
Pavi-weblogix <pavi@marketplacenetwork.com.au>
2026-04-21 14:20:13 +0530

    run build errro fixed

--- src/app/suburbs/page.tsx
+++ src/app/suburbs/page.tsx
@@ -1,7 +1,9 @@
 import Suburbs from "@/components/suburbs/Suburbs";
 
 export default function SuburbsPage() {
-  return <Suburbs />;
+  return
+  <></>
+  //  <Suburbs />;
 }
 
 // The only change is the name of the component from "StateNew" to "Bystate".
